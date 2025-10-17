b480ce87240befef05f9d3e4a72d5600b2ef3d8a
amirjavan094-gif <amirjavan094@gmail.com>
2025-10-17 13:51:54 +0330

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from .views import ( api_products,api_product,api_delete,api_update,api_add)
 
 urlpatterns = [
     path("products_list/",api_products,name="products_list"),
-    path("product/<int:id>",api_product,name="product"),
+    path("product/<int:id>/",api_product,name="product"),
     path("delete_product/<int:id>/",api_delete,name="delete_product"),
     path("update_product/<int:id>/",api_update,name="update_product"),
     path("add_product/",api_add,name="add_product"),
