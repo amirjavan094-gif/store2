654fade2b489dfe5047fe26554c9882302efa79c
amirjavan094-gif <amirjavan094@gmail.com>
2025-10-17 13:52:30 +0330

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from store.models import Products
 
 
-class Products_serializer(serializers.ModelSerializer):
+class ProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
         fields = "__all__"
