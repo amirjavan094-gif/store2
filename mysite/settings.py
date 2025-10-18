0391569939f768a80ce35e909164375a71c712a4
amirjavan094-gif <amirjavan094@gmail.com>
2025-10-18 16:32:34 +0330

    add JWT

--- mysite/settings.py
+++ mysite/settings.py
@@ -98,7 +98,7 @@ else:
 # ===========================
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',  # پیش‌فرض: فقط GET برای همه
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 
 urlpatterns = [
@@ -11,6 +15,8 @@ urlpatterns = [
     path('cart/',include('cart.urls')),
     path('payment/',include('payment.urls')),
     path('api/',include('api.urls')),
+    path('api/token/',TokenObtainPairView.as_view(),name ='token_obtain_pair'),
+    path('api/token/refresh/',TokenRefreshView.as_view(),name='token_refresh')
 ]
 
 if settings.DEBUG:
